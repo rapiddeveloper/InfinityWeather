07052cfa53917d5a8106ca0dbc3963c983259a00
rapiddeveloper <77062058+rapiddeveloper@users.noreply.github.com>
2025-10-06 16:40:47 +0100

    Initial Commit

---
+++ InfinityWeather/InfinityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityWeatherApp.swift
+//  InfinityWeather
+//
+//  Created by Orisajobi Akinbola on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct InfinityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
