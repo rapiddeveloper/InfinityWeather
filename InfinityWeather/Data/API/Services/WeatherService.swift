6f556831d2cb4347ed171c73f3dae3e4dd79492e
rapiddeveloper <77062058+rapiddeveloper@users.noreply.github.com>
2025-10-08 11:47:10 +0100

    remove comments

--- InfinityWeather/Data/API/Services/WeatherService.swift
+++ InfinityWeather/Data/API/Services/WeatherService.swift
@@ -30,14 +30,7 @@ class WeatherService: HTTPClient, WeatherServiceProtocol {
         guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "OPEN_WEATHER_API_KEY") as? String else {
            fatalError("API_KEY not found in Info.plist")
         }
-        /*
-        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String else {
-           fatalError("BASE_URL not found in Info.plist")
-        }
-        print("API Key: \(apiKey)")
-        print(baseURL)
-        self.baseURL = baseURL
-         */
+         
         self.apiKey = apiKey
     }
 
